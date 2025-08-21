1f3dba6cfbf63234d5973874859ff4da81f8711c
yih5025 <yih5025@naver.com>
2025-08-21 22:32:06 +0900

    backend: update 2025-08-21 22:32:06

--- app/services/company_news_service.py
+++ app/services/company_news_service.py
@@ -4,7 +4,7 @@ from typing import List, Dict, Optional, Tuple
 from datetime import datetime, timedelta
 
 from app.models.company_news_model import CompanyNews
-from app.models.top_gainers_model import TopGainers
+from app.models.topgainers_model import TopGainers
 
 class CompanyNewsService:
     """
