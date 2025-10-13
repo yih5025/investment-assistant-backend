793d0a6f74e79e39490836a3dd2b4b828c823b64
yih5025 <yih5025@naver.com>
2025-10-13 13:37:43 +0900

    backend: update 2025-10-13 13:37:43

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,21 +1,14 @@
 from .base_websocket_schema import WebSocketMessageType, SubscriptionType
-from .topgainers_schema import (
-    TopGainerData, TopGainersUpdateMessage,
-    TopGainersListResponse, TopGainersCategoryResponse, TopGainersSymbolResponse
-)
 from .crypto_schema import CryptoData, CryptoUpdateMessage  
 from .sp500_schema import StockInfo, SP500UpdateMessage
+from .etf_schema import ETFUpdateMessage
 
 __all__ = [
     "WebSocketMessageType",
     "SubscriptionType", 
-    "TopGainerData",
-    "TopGainersUpdateMessage",
-    "TopGainersListResponse",
-    "TopGainersCategoryResponse", 
-    "TopGainersSymbolResponse",
     "CryptoData", 
     "CryptoUpdateMessage",
     "StockInfo",
-    "SP500UpdateMessage"
+    "SP500UpdateMessage",
+    "ETFUpdateMessage"
 ]
